1e6ef3587240a28d9d5c31cb11536c83b221fa00
nawter <zaiddawoodissa@gmail.com>
2018-11-01 21:25:47 +0100

    include all the new files

---
+++ README.md
@@ -0,0 +1,2 @@
+
+## The tiny_blokchain is based on Simple Blockchain in five minutes by Siraj Raval
---
+++ scrip.sh
@@ -0,0 +1,7 @@
+o "# PythonBlockchain" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Nawter/PythonBlockchain.git
+git push -u origin master
+
