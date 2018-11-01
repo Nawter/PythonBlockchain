abc503f5a3ceae5a6e545a6c206cf2017074027b
nawter <zaiddawoodissa@gmail.com>
2018-11-01 21:27:32 +0100

    after delete some files

--- scrip.sh
+++
@@ -1,7 +0,0 @@
-o "# PythonBlockchain" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Nawter/PythonBlockchain.git
-git push -u origin master
-
